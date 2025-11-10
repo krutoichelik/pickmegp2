72f39c4b222196c6421ba70ff81af5cbfcc06c0d
maksimsuvorov <suvorovmaxim11@mail.ru>
2025-11-10 09:51:36 +0300

    sozdal paring.py

---
+++ parsing.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
